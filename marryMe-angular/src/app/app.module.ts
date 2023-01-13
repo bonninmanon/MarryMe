d7dbf1ed107c7abcd1e3dbde29594200d839db8c
manonbonnin <manonbonnin@yahoo.fr>
2023-01-13 12:01:59 +0100

    components

--- marryMe-angular/src/app/app.module.ts
+++ marryMe-angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { RobeComponent } from './robe/robe.component';
+import { CakeComponent } from './cake/cake.component';
+import { PanierComponent } from './panier/panier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    AccueilComponent,
+    RobeComponent,
+    CakeComponent,
+    PanierComponent
   ],
   imports: [
     BrowserModule,
---
+++ marryMe-angular/src/app/robe/robe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-robe',
+  templateUrl: './robe.component.html',
+  styleUrls: ['./robe.component.scss']
+})
+export class RobeComponent {
+
+}
