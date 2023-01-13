2bf84326a6b2fc040409d9d3dfc5c4fdb7427ad0
meher hraieb <hraieb@gmail.com>
2023-01-13 14:13:18 +0100

    modification class compte" type de compte admin ou client"

--- marryMe-angular/src/app/connexion/connexion-http.service.ts
+++ marryMe-angular/src/app/connexion/connexion-http.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppConfigService } from '../app-config.service';
+import { Connexion } from '../model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnexionHttpService {
+a
+  serviceUrl: string;
+  connexions: Connexion = new Connexion;
 
-  constructor() { }
-}
+  constructor(private http: HttpClient, private appConfig: AppConfigService) {
+    this.serviceUrl = appConfig.backEndUrl + "comptes/";
+  
+   }
+
+
+  findByMailAndPassword(mail: String, password:string): Observable<Connexion> {
+  
+    return this.http.post<Connexion>(this.serviceUrl + "auth", {
+      "mdp": mdp,
+      "password": password
+    });
+  }
+
+ 
+  }
+
