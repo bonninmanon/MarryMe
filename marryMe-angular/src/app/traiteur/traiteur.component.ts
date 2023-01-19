794d1cd031e9b5dc161ab5ed149c8b3168de714c
Khamad Taher <attaherikhiri@yahoo.fr>
2023-01-19 19:27:46 +0100

    final traiteur+salle

--- marryMe-angular/src/app/traiteur/traiteur.component.ts
+++ marryMe-angular/src/app/traiteur/traiteur.component.ts
@@ -39,6 +39,15 @@ export class TraiteurComponent {
   isShowInfo113: boolean;
   isShowInfo114: boolean;
   isShowInfo115: boolean;
+ 
+
+  isSizeError110: boolean;
+  isSizeError111: boolean;
+  isSizeError112: boolean;
+  isSizeError113: boolean;
+  isSizeError114: boolean;
+  isSizeError115: boolean;
+
 
   constructor(private tS: TraiteurHttpService, private router: Router, private route: ActivatedRoute){
     this.route.params.subscribe(params => { 
@@ -49,24 +58,24 @@ export class TraiteurComponent {
     });
   }
 
-  reservertraiteur()
-  {
-    console.log(this.nombreInvite);
-    if(this.nombreInvite=="10-30"){this.prix = 2500}
-    else if (this.nombreInvite=="31-60"){this.prix = 3200}
-    else if (this.nombreInvite=="61-100"){this.prix = 5000}
-    else if (this.nombreInvite==" >100 "){this.prix = 7500}
-    //this.prix = (this.nombreInvite=="petit")? 250 : 400;
+  //reservertraiteur()
+ // {
+    //console.log(this.nombreInvite);
+  //  if(this.nombreInvite=="10-30"){this.prix = 2500}
+   // else if (this.nombreInvite=="31-60"){this.prix = 3200}
+   //* else if (this.nombreInvite=="61-100"){this.prix = 5000}
+    //else if (this.nombreInvite==" >100 "){this.prix = 7500}
+   // //this.prix = (this.nombreInvite=="petit")? 250 : 400;
  
     
-   console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" + " design:" +this.cuisine + this.nombreInvite  )
+  // console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" + " design:" +this.cuisine + this.nombreInvite  )
   
-    this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
-  }
+  //  this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
+ // }
  
-  choixTraiteur(designImgClic : string) {
-    this.design=designImgClic;
-}
+ // choixTraiteur(designImgClic : string) {
+ //   this.design=designImgClic;
+//}
 
 
   Lovely(){
@@ -75,7 +84,13 @@ export class TraiteurComponent {
       this.vinDHonneur="vinDHonneur";
       this.cuisine = this.cuisineLovely;
 
-    
+      if (!this.cuisine) {
+        this.isSizeError110 = true;
+        return;
+      }
+
+      console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" +  this.nombreInvite  + "cuisine:"   +this.cuisine )
+      this.isShowInfo110 = true;
       this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
 
   }
@@ -84,7 +99,14 @@ export class TraiteurComponent {
       this.prix= 3500 ;
       this.vinDHonneur="vinDHonneur";
       this.cuisine = this.cuisinejoliesnoces;
+
+      if (!this.cuisine) {
+        this.isSizeError111 = true;
+        return;
+      }
     
+      console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" +  this.nombreInvite  + "cuisine:"   +this.cuisine )
+      this.isShowInfo111 = true;
       this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
   }
 
@@ -93,6 +115,13 @@ export class TraiteurComponent {
       this.vinDHonneur="vinDHonneur";
       this.cuisine = this.cuisineinspiration;
     
+      if (!this.cuisine) {
+        this.isSizeError112 = true;
+        return;
+      }
+
+      console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" + " design:" +this.cuisine + this.nombreInvite  )
+      this.isShowInfo112 = true;
       this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
   }
 
@@ -100,7 +129,14 @@ export class TraiteurComponent {
       this.prix= 1500 ;
       this.vinDHonneur="vinDHonneur";
       this.cuisine = this.cuisinedecontract;
-    
+
+      if (!this.cuisine) {
+        this.isSizeError113 = true;
+        return;
+      }
+
+      console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" + " design:" +this.cuisine + this.nombreInvite  )
+      this.isShowInfo113 = true;
       this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
   }
 
@@ -108,15 +144,29 @@ export class TraiteurComponent {
       this.prix= 3456 ;
       this.vinDHonneur="vinDHonneur";
       this.cuisine = this.cuisinegardenparty;
+
+      if (!this.cuisine) {
+        this.isSizeError114 = true;
+        return;
+      }
     
+      console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" + " design:" +this.cuisine + this.nombreInvite  )
+      this.isShowInfo114 = true;
       this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
   }
 
   chichampete(){
-      this.prix= 1234 ;
+      this.prix= 6300 ;
       this.vinDHonneur="vinDHonneur";
       this.cuisine = this.cuisinechichampete;
-    
+
+      if (!this.cuisine) {
+        this.isSizeError115 = true;
+        return;
+      }
+
+      console.log("prix:" +this.prix  + " vinDHonneur:" + this.vinDHonneur+ " nombreInvite:" + " design:" +this.cuisine + this.nombreInvite  )
+      this.isShowInfo115 = true;
       this.tS.create(this.prix, (this.vinDHonneur == "oui" ? true : false), this.cuisine,this.nombreInvite).subscribe(resp => {this.connecte = resp;})
   }
 }
